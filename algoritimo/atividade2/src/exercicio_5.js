af0b590e8e494aa088b64212f6db4fa031b2466e
Henrique Bueno <buenohenrique312007@gmail.com>
2026-03-09 15:08:14 -0300

    atividade 2

---
+++ algoritimo/atividade2/src/exercicio_5.js
@@ -0,0 +1,8 @@
+prompt = require('prompt-sync')();
+
+let placa = prompt("Entre com a placa:");
+for(let i = 0; i < placa.length ; i++){
+if(placa.charCodeAt(i) >= 48 && placa.charCodeAt(i) <= 57){
+    console.log(placa.charCodeAt(i));
+}
+}
