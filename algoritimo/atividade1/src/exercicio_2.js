ebb55bf75c718513c264f73f4e33855643caac76
Henrique Bueno <buenohenrique312007@gmail.com>
2026-03-08 22:30:43 -0300

    atividade 1

---
+++ algoritimo/atividade1/src/exercicio_2.js
@@ -0,0 +1,10 @@
+prompt = require('prompt-sync')();
+
+let entrada = prompt("Entre com a idade:");
+entrada = parseInt(entrada);;
+if( entrada < 18 ){
+console.log("de menor");
+}
+else{
+console.log("de maior");
+}
