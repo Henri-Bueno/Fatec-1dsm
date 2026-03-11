da1fbf25214355a01d3700422e5b3f5129945916
Henrique <buenohenrique312007@gmail.com>
2026-03-11 20:47:36 -0300

    aula 5

---
+++ algoritimo/aula5/src/exemplo_3.js
@@ -0,0 +1,4 @@
+const idade = 10;
+if( true ){
+idade = 11; // gera erro: TypeError: Assignment to constant variable.
+}
---
+++ algoritimo/aula5/src/exemplo_4.js
@@ -0,0 +1,9 @@
+const pessoa = {nome:"Ana", idade:20, peso:70.5, doador: true};
+
+console.log("nome: " + pessoa.nome);
+console.log("idade: " + pessoa.idade);
+console.log("peso: " + pessoa.peso);
+console.log("doador: " + pessoa.doador);
+
+console.log(`Nome: ${pessoa.nome}, Idade: ${pessoa.idade}, Peso: ${pessoa.peso}, Doador: ${pessoa.doador}`); //imprime
+
