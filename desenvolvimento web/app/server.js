a9ecf17dd900637e35366dc6f664c876cc147fa5
Henrique Bueno <buenohenrique312007@gmail.com>
2026-03-13 22:37:13 -0300

    aula 2

--- desenvolvimento web/app/server.js
+++ desenvolvimento web/app/server.js
@@ -11,12 +11,14 @@ dotenv.config({quiet:true});
 // process.env é ambiente do sistema
 const PORT = process.env.PORT;
 
+// construir o servidor
+const app = express();
+
 const publicPath = path.join(__dirname,"public");
 const pagesPath = path.join(publicPath,"pages");
 
-console.log("pagesPath", pagesPath);
-// construir o servidor
-const app = express();
+// mapeando a pasta /assets para a URL /arley
+app.use("/assets", express.static(path.join(publicPath,"assets")));
 
 // subir o servidor
 app.listen(PORT, function(){
@@ -26,3 +28,4 @@ app.listen(PORT, function(){
 app.get("/", function(_req,res){
     res.sendFile( path.join(pagesPath,"index.html") );
 });
+
