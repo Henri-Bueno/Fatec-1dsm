32be425fbe943e7bdec25f3064a4299e1e0756eb
Henrique Bueno <buenohenrique312007@gmail.com>
2026-03-19 13:17:35 -0300

    atividade 2

--- algoritimo/aula6/app/src/server.js
+++ algoritimo/aula6/app/src/server.js
@@ -1,5 +1,6 @@
 const path = require("path");
 const dotenv = require("dotenv");
+
 dotenv.config({
   quiet: true,
   path: path.resolve(__dirname, "..", ".env"),
@@ -8,7 +9,9 @@ dotenv.config({
 const router = require("./routes/user.routes")
 
 const express = require("express");
+
 const app = express();
+
 app.use(express.json())
 
 const PORT = process.env.PORT;
@@ -20,6 +23,8 @@ app.listen(PORT, function () {
 const publicPath = path.join(__dirname, "..", "public");
 const pagesPath = path.join(publicPath, "pages");
 const assetsPath = path.join(publicPath, "assets");
+
+//criando uma rota para a pasta pages
 
 app.use(express.static(pagesPath));
 app.use("/assets", express.static(assetsPath))
