6b1d1ea3d45f806de1079e0fd0a1bce6722c9ad7
Henrique Bueno <buenohenrique312007@gmail.com>
2026-03-21 20:19:14 -0300

    atividade 2

--- desenvolvimento web/Atividade 2/app/public/assets/js/main.js
+++ desenvolvimento web/Atividade 2/app/public/assets/js/main.js
@@ -2,7 +2,7 @@ const input = document.getElementById('search-input');
 const result = document.getElementById('result');
 
 function showResult(message, type) {
-    result.textContent = message;
+    result.innerHTML = message;
     result.className = `result ${type}`;
 }
 
@@ -12,16 +12,39 @@ async function handleCapSearch() {
     if (rawCep.length !== 8) {
         showResult("CEP inválido. Por favor, insira 8 números (ex: 12345678 ou 12.345-678).", "error");
     } else {
+        showResult("Consultando CEP...", "loading");
         const response = await fetch(`https://viacep.com.br/ws/${rawCep}/json/`)
         if (!response.ok) {
             showResult("Falha no serviço.", "error");
         } else {
-            console.log("ok");
+            const data = await response.json();
+            if (data.erro) {
+                showResult("CEP não encontrado.", "error");
+            } else {
+            const message = 
+            `<p><strong>CEP:</strong> ${data.cep || 'Não informado'}</p>
+            <p><strong>Logradouro:</strong> ${data.logradouro || 'Não informado'}</p>
+            <p><strong>Complemento:</strong> ${data.complemento || 'Não informado'}</p>
+            <p><strong>Bairro:</strong> ${data.bairro || 'Não informado'}</p>
+            <p><strong>Cidade:</strong> ${data.localidade || 'Não informado'}</p>
+            <p><strong>UF:</strong> ${data.uf || 'Não informado'}</p>
+            <p><strong>Estado:</strong> ${data.estado || 'Não informado'}</p>
+            <p><strong>Região:</strong> ${data.regiao || 'Não informado'}</p>
+            <p><strong>DDD:</strong> ${data.ddd || 'Não informado'}</p>`;
+            showResult(message, "success");
+            }   
         }
     }
 }
 input.addEventListener('keydown', (event) => {
     if (event.key === 'Enter') {
         handleCapSearch();
+    }
+});
+
+input.addEventListener('input', function() {
+    if (input.value.trim() == "") {
+     result.innerHTML = "";
+    result.className = `result`
     }
 });
