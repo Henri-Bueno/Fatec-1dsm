d98820d30f522b2b48d64343929e3937a9e46561
Henrique Bueno <buenohenrique312007@gmail.com>
2026-03-23 18:55:09 -0300

    atividade 3

--- algoritimo/aula6/app/debug-env.js
+++
@@ -1,1 +0,0 @@
-require(" dotenv\).config(); console.log(\env password:\, JSON.stringify(process.env.POSTGRES_PASSWORD));
--- algoritimo/aula6/app/test-pg.js
+++
@@ -1,3 +0,0 @@
-const { Pool } = require('pg');
-const pool = new Pool({ host: 'localhost', user: 'postgres', password: '123', database: 'bdaula', port: 5432 });
-pool.query('SELECT 1').then(r=>{console.log('ok', r.rows); pool.end();}).catch(e=>{console.error('err', e); pool.end();});
