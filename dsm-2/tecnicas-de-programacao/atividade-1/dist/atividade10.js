feb40aef0b85a514548fb53e7e01afbb5097d769
Henrique Bueno <buenohenrique312007@gmail.com>
2026-08-19 14:01:45 -0300

    renomeando arquivos do dsm-1

---
+++ dsm-2/tecnicas-de-programacao/atividade-1/dist/atividade10.js
@@ -0,0 +1,13 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+const pessoa = {
+    nome: "Ana",
+    idade: 21,
+    peso: 61.23
+};
+const carro = {
+    modelo: "Gol",
+    ano: 2010,
+    marca: "VW"
+};
+//# sourceMappingURL=atividade10.js.map
